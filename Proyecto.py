d2fd14e7c4f7eb031bfae9056ea2afd47168c0b9
Eliomar-Rodriguez <Capitan1>
2016-03-13 14:02:14 -0600

    un archivo como para hacer pruebas

--- Proyecto.py
+++ Proyecto.py
@@ -4,11 +4,12 @@ def login():
     user=[[2016108660,"Eliomar","Rodriguez","Funcionario","gato"],[2016108661,"Steven","Peraza","Estudiante"]]
     i=0
     j=0
-    while True:
-        while i!=0:
-            while j!=0:
-                if usuario==user[i][0] and password==user[i][4]:
-                    print(user[i][0])
-        i+=1
-        j=0
+   # while True:
+    while i!=0:
+        while j!=0:
+            if usuario==user[i][0] and password==user[i][4]:
+                print(user[i][0])
+            j+=1
+    i+=1
+    j=0
 login()
