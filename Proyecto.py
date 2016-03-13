c733934bef3042cbc7233324f49efe0cb656bf2a
Eliomar-Rodriguez <Capitan1>
2016-03-13 13:26:07 -0600

    Proceso de crear el login mediante el uso de matrices

---
+++ Proyecto.py
@@ -0,0 +1,14 @@
+def login():
+    usuario=input("Digite el usuario")
+    password=input("Digite la contraseña")
+        =[[2016108660,"Eliomar","Rodriguez","Funcionario","gato"],[2016108661,"Steven","Peraza","Estudiante"]]
+        i=0
+        j=0
+    while True:
+        while i!=0:
+            while j!=0:
+                if usuario==usuarios[i][0] and password==usuarios[i][4]:
+                print(usuarios[i][0])
+        i+=1
+        j=0
+login()
