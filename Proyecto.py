4dc59a7f9ad1dbfa69149cfba4ab815f2610c8f0
Eliomar-Rodriguez <Capitan1>
2016-03-13 12:12:47 -0600

    Eliminé todo para ya empezar desde cero con la progra.

--- Proyecto.py
+++
@@ -1,8 +0,0 @@
-n1=int(input("Digite el primer numero"))
-n2=int(input("Digite el segundo numero"))
-def calc(n1,n2):
-    tot=1
-    tot=n1+n2
-    return tot
-print(calc(n1,n2))
-print("la puta madre!!!!")
