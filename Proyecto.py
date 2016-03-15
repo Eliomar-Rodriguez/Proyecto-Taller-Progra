a5ab94574fb21b9d71a34b183b55fee13100fc8c
Eliomar-Rodriguez <Capitan1>
2016-03-15 10:58:54 -0600

    Finalización del logup y del menú principal, el login esta en proceso

--- Proyecto.py
+++ Proyecto.py
@@ -1,25 +1,39 @@
-
-def logout():
-    carne=int(input("Digite el número de carnet."))
-    name=int(input("Digite su nombre completo."))
-    mail=int(input("Digite su correo electrónico."))
-    contra=int(input("Digite la contraseña que desea utilizar."))
-    users.append([carne,name,mail,contra])
-
+def menu_profes():
+    print("")
 
 def login():
-    print("Hola")
+    print("")
 
-print("""                 Menú de inico de sesión
+def logup():
+    print("""Digite el tipo de cuenta que va a crear
+                                                    1. Estudiante
+                                                    2. Profesor""")
+    tipo=int(input("Digite el tipo de cuenta que va a crear"))
+    carne=int(input("Digite el número de carnet."))
+    name=input("Digite su nombre completo.")
+    mail=input("Digite su correo electrónico.")
+    contra=input("Digite la contraseña que desea utilizar.")
+
+    usuarios=[]
+    usuarios.append([carne,name,mail,contra,tipo])
+
+def menu_principal():
+    print("""                 Menú de inico de sesión
                     1. Iniciar sesión.
                     2. Crear Cuenta.
+                    3. Salir
     """)
-op=int(input("Digite la opción a elegir."))
-if op==1:
-    login()
-elif op==2:
-    logout()
-else:
-    print("Error en los datos")
+    op=int(input("Digite la opción a elegir."))
+    if op==1:
+        login()
+    elif op==2:
+        logup()
+    elif op==3:
+        quit()
+    else:
+        print("Error en los datos, debe de digitar una opción válida.")
 
+def menu_estudiantes():
+    print("")
 
+menu_principal()
--- probar.py
+++ probar.py
@@ -10,4 +10,15 @@ def logo():
         *          *           *          *          *
         *            *  *   *                *   *
         """)
-logo()
+
+print("""Digite el tipo de cuenta que va a crear
+                                                    1. Estudiante
+                                                    2. Profesor""")
+tipo=int(input("Digite el tipo de cuenta que va a crear"))
+carne=int(input("Digite el número de carnet."))
+name=input("Digite su nombre completo.")
+mail=int(input("Digite su correo electrónico."))
+contra=int(input("Digite la contraseña que desea utilizar."))
+
+usuarios=[]
+usuarios.append([carne,name,mail,contra]) #listo
