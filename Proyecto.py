5eb7e6efc264fa1a5679aadf4bdac2d53b153985
Eliomar-Rodriguez <Capitan1>
2016-03-15 09:42:28 -0600

    Creación del login (Inicio de sesión con una cuenta ya existente) y logup (Creación de una cuenta)

--- Proyecto.py
+++ Proyecto.py
@@ -1,15 +1,25 @@
+
+def logout():
+    carne=int(input("Digite el número de carnet."))
+    name=int(input("Digite su nombre completo."))
+    mail=int(input("Digite su correo electrónico."))
+    contra=int(input("Digite la contraseña que desea utilizar."))
+    users.append([carne,name,mail,contra])
+
+
 def login():
-    usuario=input("Digite el usuario")
-    password=input("Digite la contraseña")
-    user=[[2016108660,"Eliomar","Rodriguez","Funcionario","gato"],[2016108661,"Steven","Peraza","Estudiante"]]
-    i=0
-    j=0
-   # while True:
-    while i!=0:
-        while j!=0:
-            if usuario==user[i][0] and password==user[i][4]:
-                print(user[i][0])
-            j+=1
-    i+=1
-    j=0
-login()
+    print("Hola")
+
+print("""                 Menú de inico de sesión
+                    1. Iniciar sesión.
+                    2. Crear Cuenta.
+    """)
+op=int(input("Digite la opción a elegir."))
+if op==1:
+    login()
+elif op==2:
+    logout()
+else:
+    print("Error en los datos")
+
+
