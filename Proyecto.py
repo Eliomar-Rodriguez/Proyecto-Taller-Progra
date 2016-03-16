2ad6b4033a4bb1a246da3fa6da5fbef7ce126ded
Eliomar-Rodriguez <Capitan1>
2016-03-16 15:10:50 -0600

    Login esta en proceso!

--- Proyecto.py
+++ Proyecto.py
@@ -1,12 +1,6 @@
 def menu_profes():
     print("")
 
-def login(): ################################### PENDIENTE  ##################################
-    user=input("Digite su usuario")
-    password=input("Digite la contraseña")
-    i=0
-    j=0
-    if user=usuarios[i][0] and password=usuarios[i][3]:
 
 def logup():
     print("""Digite el tipo de cuenta que va a crear
@@ -20,6 +14,17 @@ def logup():
 
     usuarios=[]
     usuarios.append([carne,name,mail,contra,tipo])
+    print("El proceso de registro se realizó correctamente.")
+    menu_principal()
+    return usuarios
+
+def login(usuarios): ################################### PENDIENTE  ##################################
+    user=input("Digite su usuario")
+    password=input("Digite la contraseña")
+    i=0
+    j=0
+    if user==usuarios[i][0] and password==usuarios[i][3]:
+        print("correcto")
 
 def menu_principal():
     print("""                 Menú de inico de sesión
